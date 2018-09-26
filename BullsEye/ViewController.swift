f4cd48d93c57af15ae2b27ef80de188cd7168e4a
Katherine <katherineli0514@gmail.com>
2018-09-25 22:08:37 -0700

    Set up the project.

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Han Li on 9/25/18.
+//  Copyright © 2018 katherine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
